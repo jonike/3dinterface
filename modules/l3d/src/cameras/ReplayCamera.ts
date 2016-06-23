78db2036d45d2209b5ef2b3e967998236496d65d
Thomas FORGIONE <thomas.forgione@gmail.com>
2016-06-23 10:36:42 +0200

    ReplayCamera is cleaned, it compiles...
    but its wrong on so many leves

--- modules/l3d/src/cameras/ReplayCamera.ts
+++ modules/l3d/src/cameras/ReplayCamera.ts
@@ -3,10 +3,11 @@ import * as THREE from 'three';
 import { BaseCamera } from './BaseCamera';
 import { CameraItf } from '../utils/Logger';
 import { BaseRecommendation } from '../recommendations/BaseRecommendation';
+import { SphericCamera } from './SphericCamera';
 
 module l3d {
 
-    export class ReplayCamera extends BaseCamera {
+    export class ReplayCamera extends SphericCamera {
 
         coins : any;
 
@@ -17,28 +18,6 @@ module l3d {
 
         counter : number;
 
-        position : THREE.Vector3;
-
-        target : THREE.Vector3;
-
-        newPosition : THREE.Vector3;
-
-        newTarget : THREE.Vector3;
-
-        oldPosition : THREE.Vector3;
-
-        oldTarget : THREE.Vector3;
-
-        moving : boolean;
-        movingHermite : boolean;
-
-        hermitePosition : any;
-        hermiteAngles : any;
-
-        forward : THREE.Vector3;
-
-        t : number;
-
         data : any;
 
         callback : () => any;
@@ -47,9 +26,6 @@ module l3d {
         recovering : boolean;
 
         path : any;
-
-        theta : number;
-        phi : number;
 
         shouldRecover : boolean;
 
@@ -90,20 +66,11 @@ module l3d {
             this.started = false;
             this.counter = 0;
 
-            this.position = new THREE.Vector3();
-            this.target = new THREE.Vector3();
-            this.newPosition = new THREE.Vector3();
-            this.newTarget = new THREE.Vector3();
-
             this.data = arguments[5];
             this.callback = arguments[6];
 
             this.started = true;
             this.path = this.data.events;
-
-            // Set Position
-            this.theta = Math.PI;
-            this.phi = Math.PI;
 
             this.shouldRecover = false;
 
@@ -128,69 +95,20 @@ module l3d {
         }
 
         update(time : number) : boolean {
+
+            super.update(time);
+
             this.totalTime += time;
+
             if (this.totalTime > this.quittingTime) {
                 process.exit(0);
             }
+
             if (this.started) {
-                if (this.event.type == 'camera') {
-                    this.cameraMotion(time);
-                } else if (this.event.type == 'previousnext') {
-                    this.linearMotion(time / 5);
-                } else if (this.event.type == 'arrow') {
-                    this.hermiteMotion(time);
-                }
-                // } else if (this.event.type == 'coin') {
-                //     // Nothing to do
-                // } else if (this.event.type == 'reset') {
-                //     // Nothing to do
-                // }
+
             }
 
             return this.finished;
-        }
-
-        linearMotion(time : number) {
-            var tmp = mth.sum(mth.mul(this.oldPosition, 1-this.t), mth.mul(this.newPosition, this.t));
-            this.position.copy(tmp);
-            this.t += 0.1 * time / 20;
-
-            if (this.t > 1) {
-                this.nextEvent();
-            }
-        }
-
-        cameraMotion(time : number) {
-
-            var tmp = mth.sum(mth.mul(this.oldPosition, 1-this.t), mth.mul(this.newPosition, this.t));
-            this.position.copy(tmp);
-            this.target = mth.sum(mth.mul(this.oldTarget, 1-this.t), mth.mul(this.newTarget, this.t));
-            this.t += this.recovering ? 0.01 : 1 / (((new Date(this.path[this.counter].time)).getTime() - (new Date(this.path[this.counter-1].time)).getTime()) / 20);
-
-            if (this.t > 1) {
-                this.recommendationClicked = null;
-                if (typeof this.recoverCallback === 'function') {
-                    this.recoverCallback();
-                    this.recoverCallback = null;
-                } else {
-                    this.nextEvent();
-                }
-
-            }
-        }
-
-        hermiteMotion(time : number) {
-            var e = this.hermitePosition.eval(this.t);
-            this.position.copy(e);
-
-            this.target = mth.sum(this.position, this.hermiteAngles.eval(this.t));
-
-            this.t += 0.01 * time / 20;
-
-            if (this.t > 1) {
-                this.recommendationClicked = null;
-                this.nextEvent();
-            }
         }
 
         nextEvent() {
@@ -223,7 +141,7 @@ module l3d {
             // console.log(this.event.type);
 
             if (this.event.type == 'camera') {
-                this.move(this.event);
+                this.startLinearMotion(this.event);
             } else if (this.event.type == 'coin') {
                 // Get the coin with the same id of event
                 for (var i = 0; i < this.coins.length; i++) {
@@ -250,14 +168,14 @@ module l3d {
                     (function(self : ReplayCamera, tmp : number) {
                         self.event.type = 'camera';
                         self.recovering = true;
-                        self.move({
+                        self.startLinearMotion({
                             position: self.position.clone(),
                             target: self.cameras[self.event.id].camera.position.clone()
-                        }, function() {
+                        }/*, function() {
                             self.recovering = false;
                             self.event.type = 'arrow';
                             self.moveReco(tmp);
-                        });
+                        }*/);
                     })(this, this.event.id);
                 } else {
                     this.moveReco(this.event.id);
@@ -271,7 +189,7 @@ module l3d {
                 //    },500);
                 //})(this);
             } else if (this.event.type == 'previousnext') {
-                this.move(this.event);
+                this.startLinearMotion(this.event);
             } else {
                 // Ignore other events
                 this.nextEvent();
@@ -280,8 +198,6 @@ module l3d {
 
         reset() {
             this.resetPosition();
-            this.moving = false;
-            this.movingHermite = false;
         }
 
         resetPosition() {
@@ -290,78 +206,12 @@ module l3d {
             this.anglesFromVectors();
         }
 
-        vectorsFromAngles() {
-            // Update direction
-            this.forward.y = Math.sin(this.phi);
-
-            var cos = Math.cos(this.phi);
-            this.forward.z = cos * Math.cos(this.theta);
-            this.forward.x = cos * Math.sin(this.theta);
-            this.forward.normalize();
-        }
-
-        anglesFromVectors() {
-            // Update phi and theta so that return to reality does not hurt
-            var forward = mth.diff(this.target, this.position);
-            forward.normalize();
-
-            this.phi = Math.asin(forward.y);
-
-            // Don't know why this line works... But thanks Thierry-san and
-            // Bastien because it seems to work...
-            this.theta = Math.atan2(forward.x, forward.z);
-        }
-
-        move(recommendation : BaseRecommendation | CameraItf, callback ?: () => any) {
-
-            if (typeof callback === 'function') {
-
-                this.recoverCallback = callback;
-
-            }
-
-            var otherCamera : CameraItf = (<BaseRecommendation>recommendation).camera || <CameraItf>recommendation;
-
-            this.moving = true;
-            this.oldTarget =   this.target.clone();
-            this.oldPosition = this.position.clone();
-            this.newTarget =   new THREE.Vector3(otherCamera.target.x, otherCamera.target.y, otherCamera.target.z);
-            this.newPosition = new THREE.Vector3(otherCamera.position.x, otherCamera.position.y, otherCamera.position.z);
-
-            this.t = 0;
-
-        }
-
-        moveHermite(recommendation : BaseRecommendation | CameraItf) {
-
-            if (this.shouldRecover === false) {
-                this.shouldRecover = true;
-            }
-
-            var otherCamera  : CameraItf = (<BaseRecommendation>recommendation).camera || <CameraItf>recommendation;
-
-            this.movingHermite = true;
-            this.t = 0;
-
-            this.hermitePosition = new mth.Hermite.special.Polynom(
-                mth.copy(this.position),
-                mth.copy(otherCamera.position),
-                mth.mul(mth.diff(otherCamera.target, otherCamera.position).normalize(),4)
-            );
-
-            this.hermiteAngles = new mth.Hermite.special.Polynom(
-                mth.diff(this.target, this.position),
-                mth.diff(otherCamera.target, otherCamera.position),
-                new THREE.Vector3()
-            );
-        }
-
         moveReco(recommendationId : number) {
 
             this.recommendationClicked = recommendationId;
 
             // process.stderr.write('Moving to ' + JSON.stringify(this.cameras[recommendationId].camera.position) + '\n');
-            this.moveHermite(this.cameras[recommendationId]);
+            this.startHermiteMotion(this.cameras[recommendationId].camera);
 
         }
 
