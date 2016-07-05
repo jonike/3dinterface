6d33da36b2858ac35f55eb808e715c95996ba5a8
Thomas FORGIONE <tforgione@users.noreply.github.com>
2016-07-05 09:49:13 +0200

    Removed echo in update-typings commands generation

--- utils/update-typings.sh
+++ utils/update-typings.sh
@@ -2,7 +2,7 @@
 for module in `find $(pwd)/../modules -maxdepth 3 -name "typings.json"`; do
     cd ${module%/*}
     for typing in `cat typings.json | jq '.globalDependencies' | jq '.[]' | tr -d '"'`; do
-        echo typings install --save --global dt~`echo $typing | cut -d '/' -f 2 | cut -d '#' -f 1`
+        typings install --save --global dt~`echo $typing | cut -d '/' -f 2 | cut -d '#' -f 1`
     done
 done
 
