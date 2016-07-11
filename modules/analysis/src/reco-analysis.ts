e2acffd6becb3d284804282c7e210815e2cecf49
Thomas FORGIONE <thomas.forgione@gmail.com>
2016-07-11 09:08:54 +0200

    Moving analyse function to its own file

--- modules/analysis/src/reco-analysis.ts
+++ modules/analysis/src/reco-analysis.ts
@@ -13,6 +13,7 @@ import { pixelsToFile } from './pixelsToFile';
 import { selectScene, UndefinedSceneError } from './selectScene';
 
 import { initializeScene } from './initializeScene';
+import { analyse } from './analyse';
 
 let Canvas = require('canvas');
 let gl = require('gl');
@@ -41,54 +42,6 @@ let colorToFace : THREE.Face3[] = [];
 let triangleMeshes : { [id:string] : THREE.Mesh } = {};
 
 let loader : l3d.ProgressiveLoader;
-
-export function analyse(
-    renderer : THREE.WebGLRenderer,
-    scene : THREE.Scene,
-    camera : l3d.SphericCamera) {
-
-    camera.look();
-    renderer.render(scene, camera);
-
-    let gl = renderer.getContext();
-
-    let pixels = new Uint8Array(width * height * 4);
-    gl.readPixels(0,0,width,height,gl.RGBA,gl.UNSIGNED_BYTE,pixels);
-
-    // Compute the histogram of the image
-    let histogram : number[] = [];
-
-    for (let i = 0; i < pixels.length - 3; i += 4) {
-
-        let index = pixels[i] * 256 * 256 + pixels[i+1] * 256 + pixels[i+2] - 1;
-
-        // Ignore black
-        if (index === -1) {
-            continue;
-        }
-
-        histogram[index] = histogram[index] === undefined ? 1 : histogram[index] + 1;
-
-    }
-
-    // Normalize the histogram and build the js array
-    let sum = histogram.reduce((a,b) => a+b, 0);
-    let array : {triangle:number, area:number}[] = [];
-
-    for (let i in histogram) {
-
-        array.push({
-            triangle:parseInt(i,10),
-            area:histogram[i] / sum
-        });
-
-    }
-
-    // Sort it by decreasing area
-    array.sort((a,b) => b.area - a.area);
-
-    return {array, pixels};
-}
 
 function main(configScene : config.Scene, generateImages : string, verbose : boolean) {
 
