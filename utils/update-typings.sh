2a278ebbb01fe7380be896e0ec1cd17d4ac71626
Thomas FORGIONE <thomas.forgione@gmail.com>
2016-07-11 09:15:50 +0200

    Removed version in typings.json

--- utils/update-typings.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for module in `find $(pwd)/../modules -maxdepth 3 -name "typings.json"`; do
-    cd ${module%/*}
-    for typing in `cat typings.json | jq '.globalDependencies' | jq '.[]' | tr -d '"'`; do
-        typings install --save --global dt~`echo $typing | cut -d '/' -f 2 | cut -d '#' -f 1`
-    done
-done
-
